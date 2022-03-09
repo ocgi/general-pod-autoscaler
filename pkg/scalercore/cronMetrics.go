357415fdd59693e1e0cd60e005a2c16095b809d4
twilight327426371 <yzg3662875@gmail.com>
2022-03-09 10:49:40 +0800

    fix cron set bug

--- pkg/scalercore/cronMetrics.go
+++ pkg/scalercore/cronMetrics.go
@@ -138,7 +138,7 @@ func (s *CronMetricsScaler) getFinalMatchAndMisMatch(gpa *v1alpha1.GeneralPodAut
 		break
 	}
 	// fix bug: misMatch diff s.now < 1 ,but match diff s.now > 1
-	if s.now.Sub(misMatch).Minutes() < 1 && s.now.After(misMatch) && match.Sub(s.now).Minutes() < 1 {
+	if s.now.Sub(misMatch).Minutes() <= 1 && s.now.After(misMatch) && match.Sub(s.now).Minutes() < 1 {
 		return &misMatch, &match, nil
 	}
 
